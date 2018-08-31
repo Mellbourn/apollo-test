10cee3398a3d5ab70ee51a8ea6a86f1f5c0ffe73
Klas Mellbourn <klas@mellbourn.net>
2018-08-31 18:02:22 +0200

    enumerated workflow serials

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import ApolloClient from 'apollo-boost'
 import gql from 'graphql-tag'
 import * as React from 'react'
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider, Query } from 'react-apollo'
 
 import logo from './logo.svg'
 
@@ -26,6 +26,40 @@ client
   // tslint:disable-next-line:no-console
   .then(result => console.log(result));
 
+const WorkflowSerials = () => (
+  <Query
+    query={gql`
+      {
+        allTasks {
+          nodes {
+            workflowSerial
+          }
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) {
+        return <p>Loading...</p>;
+      }
+      if (error) {
+        return <p>Error :(</p>;
+      }
+
+      let key = 0;
+      // tslint:disable-next-line:no-console
+      console.log("data", data);
+      return data.allTasks.nodes.map(
+        ({ workflowSerial }: { workflowSerial: number }) => (
+          <div key={key++}>
+            <p>{`${workflowSerial}`}</p>
+          </div>
+        )
+      );
+    }}
+  </Query>
+);
+
 class App extends React.Component {
   public render() {
     return (
@@ -33,11 +67,10 @@ class App extends React.Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">DeepSegment PoC</h1>
           </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-          </p>
+          <p className="App-intro">Workflow serials:</p>
+          <WorkflowSerials />
         </div>
       </ApolloProvider>
     );
